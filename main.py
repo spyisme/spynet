80beea169fda22a357aca997af9f6abd576f9145
spyisme <amooraymanh730072@gmail.com>
2023-12-22 12:15:18 +0000

    s

--- main.py
+++ main.py
@@ -23,5 +23,6 @@ def internal_error(error):
     return error_message, 500
 
 if __name__ == "__main__":
+    app.debug = True  
     from waitress import serve
     serve(app, host="0.0.0.0", port=80)
