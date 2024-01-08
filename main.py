c9b576c940c4371fbb0a147bc7a08ae1ac4e8455
sadaslkdashkldsacls <amrasdasdask@spysnet.com>
2024-01-08 17:25:44 +0200

    ss

--- main.py
+++ main.py
@@ -5,20 +5,6 @@ from io import StringIO
 
 app = create_app()
 
-# Create a buffer to capture console output
-console_output_buffer = StringIO()
-
-# Replace the standard output with the buffer
-sys.stdout = console_output_buffer
-
-@app.route('/console2')
-def console():
-    # Get the console output and reset the buffer
-    console_output = console_output_buffer.getvalue()
-    console_output_buffer.truncate(0)
-    console_output_buffer.seek(0)
-
-    return render_template('console.html', console_output=console_output)
 
 @app.errorhandler(404)
 def page_not_found(e):
