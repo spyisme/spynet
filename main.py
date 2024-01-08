3e025ae95c5b4b61a7023fbfa95ca9e7d227a731
sadaslkdashkldsacls <amrasdasdask@spysnet.com>
2024-01-08 17:12:38 +0200

    ss

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 from website import create_app
-from flask import  redirect 
-import logging
-
-from os import system
-system("title " + "Spynet")
-
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+from flask import redirect
 
 app = create_app()
-
 
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect('/all')
-
-
 
 @app.errorhandler(500)
 def internal_error(error):
@@ -23,6 +13,4 @@ def internal_error(error):
     return error_message, 500
 
 if __name__ == "__main__":
-    app.debug = True  
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=80  ,  _quiet=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
