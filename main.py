d969774315d7ae6de5b651366b3ebd38be40ebb0
Spyisme <amooraymanh73007@gmail.com>
2024-01-14 12:18:07 +0200

    ss

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from website import create_app
-from flask import  redirect 
+from flask import request, redirect
 import logging
+import os
 
-from os import system
-system("title " + "Spynet")
-
+# Set up logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+# Create a logger for your application
+logger = logging.getLogger('Spynet')
+logger.setLevel(logging.INFO)
+
+# Create a file handler and set the format
+log_file_path = 'access_log.txt'
+file_handler = logging.FileHandler(log_file_path)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# ...
+
 app = create_app()
 
+@app.before_request
+def log_request_info():
+    ip_address = request.headers['CF-Connecting-IP']
+    logger.info(f"IP Address: {ip_address} accessed {request.url}")
 
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect('/')
 
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    error_message = f"500 error: {str(error)}"
-    return error_message, 500
-
 if __name__ == "__main__":
-    app.debug = True  
+    app.debug = True
     from waitress import serve
-    serve(app, host="0.0.0.0", port=80  ,  _quiet=True)
+    serve(app, host="0.0.0.0", port=80, _quiet=True)
+
+
+
+
--- website/views.py
+++ website/views.py
@@ -195,15 +195,10 @@ def display_links():
 
 
 @views.route("/logs")
-def get_visitor_ip():
-    # Check if the request is coming through Cloudflare
-    if 'CF-Connecting-IP' in request.headers:
-        visitor_ip = request.headers['CF-Connecting-IP']
-    else:
-        # If not through Cloudflare, use the remote address
-        visitor_ip = request.remote_addr
-
-    return f'The visitor\'s IP address is: {visitor_ip}'
+def logs():
+    with open('access_log.txt') as f:
+        lines = f.readlines()
+    return lines
 
 
 @views.route("/")
