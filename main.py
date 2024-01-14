2f4e391f17bc3da6734a7954119f43f62bc24c23
Spyisme <amooraymanh73007@gmail.com>
2024-01-14 12:43:40 +0200

    ss

--- main.py
+++ main.py
@@ -12,21 +12,18 @@ log.setLevel(logging.ERROR)
 logger = logging.getLogger('Spynet')
 logger.setLevel(logging.INFO)
 
-
-app = create_app()
-
+# Create a file handler and set the format
 log_file_path = 'access_log.txt'
 file_handler = logging.FileHandler(log_file_path)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
-
-logger = logging.getLogger(__name__)
 logger.addHandler(file_handler)
 
-excluded_urls = {'http://spysnet.com/favicon.ico', 'http://spysnet.com/logs'}
-device_pattern = 'Mobile'  # Adjust this pattern based on your needs
-excluded_urls.add(device_pattern)
+# ...
 
+app = create_app()
+
+excluded_urls = {'http://spysnet.com/favicon.ico', 'http://spysnet.com/logs'}
 pattern = 'http://spysnet.com/static/assets/*'
 excluded_urls.add(pattern)
 
@@ -34,26 +31,14 @@ excluded_urls.add(pattern)
 def log_request_info():
     if any(fnmatch.fnmatch(request.url, pattern) for pattern in excluded_urls):
         return
-
     if 'CF-Connecting-IP' in request.headers:
         ip_address = request.headers['CF-Connecting-IP']
     else:
         ip_address = request.remote_addr
-
-    # Determine device details
     user_agent = request.user_agent
     device_details = f"Device: {user_agent.platform} - Browser: {user_agent.browser} - Version: {user_agent.version}"
 
-    log_entry = f"IP Address: {ip_address} accessed {request.url}. {device_details}"
-    
-    # Add a line of dashes before and after the log entry
-    separator = '-' * 30
-    log_entry_with_separator = f"{separator}\n{log_entry}\n{separator}"
-
-    logger.info(log_entry_with_separator)
-
-
-    
+    logger.info(f"IP Address: {ip_address} accessed {request.url} , {device_details}")
 
 @app.errorhandler(404)
 def page_not_found(e):
