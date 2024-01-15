6e5828db97fb1b04a734cf149bd84dbe65cd07ce
Spyisme <amooraymanh73007@gmail.com>
2024-01-15 15:33:43 +0200

    ss

--- main.py
+++ main.py
@@ -1,52 +1,21 @@
 from website import create_app
-from flask import request, redirect , jsonify
+from flask import  redirect 
 import logging
 
-import os 
-
-
-
-os.system('title Spynet')
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
-logger = logging.getLogger('Spynet')
-logger.setLevel(logging.INFO)
-
 
 app = create_app()
 
 
-
-# @app.before_request
-# def ip_whitelist():
-
-#     if 'static' in request.path:
-
-#         return
-
-#     with open("allowedips.txt") as file:
-#         lines = [line.rstrip() for line in file]
-
-#     if 'CF-Connecting-IP' in request.headers:
-#         ip_address = request.headers['CF-Connecting-IP']
-#     else:
-#         ip_address = request.remote_addr
-
-#     if ip_address not in lines:
-#         return jsonify({'Ip': f'{ip_address}'})
-    
-
-
 @app.errorhandler(404)
 def page_not_found(e):
-    return redirect('/')
+    return redirect('/all')
+
+
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = True  
     from waitress import serve
-    serve(app, host="0.0.0.0", port=80, _quiet=True)
-
-
-
-
+    serve(app, host="0.0.0.0", port=80)
