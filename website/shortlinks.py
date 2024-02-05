4f429230541e4edac88cefddf4cf82d53c0b7368
Spyisme <amooraymanh73007@gmail.com>
2024-02-05 19:47:51 +0200

    ss

--- website/shortlinks.py
+++ website/shortlinks.py
@@ -1,131 +1,12 @@
-import os , sys , threading
-from flask import Flask, render_template, request, redirect, url_for , Blueprint , flash ,  send_file
-import requests
-from pytube import Playlist
-import youtube_dl
-import requests
-import os
-from flask_login import login_required
-from lxml import html
-
+from flask import   redirect,  Blueprint 
 
 
 shortlinks = Blueprint('shortlinks', __name__)
 
-#Link shortner -------------------------------------------------------------------------
-
-def delete_lines_after_target_line(file_path, target_line, number):
-    try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-        for i, line in enumerate(lines):
-            if line.strip() == target_line:
-                del lines[i:i + number]
-                break
-        with open(file_path, 'w') as file:
-            file.writelines(lines)
-
-        print(f"Deleted {number} lines after '{target_line}' from {file_path}")
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-def restart_server_with_delay(restart_script_path):
-    def restart_server():
-        os.execl(sys.executable, sys.executable, restart_script_path, *sys.argv[1:])
-    restart_thread=threading.Thread(target=lambda:threading.Timer(2,restart_server).start())
-    restart_thread.start()
-#----------------------------------------------------------------------------------------
-def append_text_to_filepy(text_to_append, text2):
-    try:
-        with open("website/shortlinks.py", 'a') as file:
-            file.write(f"""
-@shortlinks.route("/{text_to_append}")
-def {text_to_append}():
-    return redirect("{text2}") 
-            """+ "\n")
-            print("New url added")
-    except FileNotFoundError:
-        print("File not found: shortlinks.py")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-
-
-
-def append_text_to_file(text_to_append, text2):
-    try:
-        with open("links.txt", 'a') as file2 :
-            file2.write(text_to_append + "\n")
-            print("New url added")
-    except FileNotFoundError:
-        print("File not found: links.txt")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-#----------------------------------------------------------------------------------------
-def search_word_in_file(target_word):
-    try:
-        with open("links.txt", 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                if target_word in line:
-                    return True
-        return False
-    except FileNotFoundError:
-        print("File not found")
-        return False
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return False
-#-------------------------------------------------------------------------------------
-@shortlinks.route("/delete")
-def delete():
-    with open('website/links.txt', 'r') as file:
-      lines = file.readlines()
-    item_id = request.args.get('id')
-    target_line = f"""@shortlinks.route("/{item_id}")"""
-    delete_lines_after_target_line("website/shortlinks.py", target_line, 3)
-    delete_lines_after_target_line("links.txt", item_id, 1)
-    restart_server_with_delay("main.py")
-    return render_template("teacher.html",alert= "true" , lines=lines, teachername="All")
-
-
-
-@shortlinks.route('/submit', methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        input1 = request.form['input1']
-        input2 = request.form['input2']
-        checkbox = request.form.getlist('check') 
-        found = search_word_in_file(input1)
-      
-        if not found:
-          if 'edit' in checkbox:
-              print(checkbox)
-              append_text_to_file(input1, input2)
-              append_text_to_filepy(input1, input2)
-              restart_server_with_delay("main.py")
-          else:
-            append_text_to_filepy(input1, input2)
-            restart_server_with_delay("main.py")
-          
-        else:
-            return redirect(url_for("views.home"))
-    return redirect(url_for("shortlinks.shlinks", input1=input1))
-
-
 #------------------------------------------------------------------------------------------------------
 
 
-@shortlinks.route('/yourlink')
-def shlinks():
-        input1 = request.args.get('input1')
-        return render_template("used_pages/index.html", done_message = "done" , input1 = input1) 
 
-@shortlinks.route("/links")
-def links():
-    return render_template("used_pages/index.html",links= "yes" )
 
 @shortlinks.route("/books")
 def books():
@@ -175,14 +56,4 @@ def vdo():
 @shortlinks.route("/germanpdfs")
 def germanpdfs():
     return redirect("https://drive.google.com/drive/folders/1Mfyook7uXVp0d3y85WaRUuXB_F6Or_Yn?usp=drive_link") 
-            
-
-@shortlinks.route("/nawarcmd")
-def nawarcmd():
-    return redirect("https://www.nawaracademy.com/photos/cmd.php") 
-            
-
-@shortlinks.route("/youtube")
-def youtube():
-    return redirect("https://www.youtube.com/") 
             
