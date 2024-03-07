3dc11c2fb0d9bcc15dc8c2bc69b5d1f3c6480488
Spyisme <amooraymanh73007@gmail.com>
2024-03-07 16:47:38 +0200

    ff

--- website/static/java/song.js
+++ website/static/java/song.js
@@ -57,17 +57,8 @@ function toggleMusic() {
 }
 
 // Add an event listener for the "click" event to toggle music
-// document.addEventListener("click", function () {
-//     var musicOn = localStorage.getItem("musicOn");
-
-//     // Check if music is set to true and play it
-//     if (musicOn === "true") {
-//         audio.volume = 0.5;
-//         audio.play();
-
-//         // Save the current time when leaving the page
-//         window.addEventListener("beforeunload", function () {
-//             localStorage.setItem("audioTime", audio.currentTime);
-//         });
-//     }
-// });
+document.addEventListener("click", function () {
+    toggleMusic();
+    // Remove the click event listener after toggling music
+    document.removeEventListener("click", toggleMusic);
+});
