7e5efd38747f0f181d9ee4d86198cc0338f8ea46
Spyisme <amooraymanh73007@gmail.com>
2024-03-07 20:46:12 +0200

    ff

--- website/views.py
+++ website/views.py
@@ -27,6 +27,105 @@ iframe_lib = "210329"
 YOUTUBE_API_KEY = 'AIzaSyDq93Og0CW8s4n4bEcfGzHut3fbB56QW64'
 
 
+
+
+def convert_duration(duration):
+    duration = duration[2:] 
+    hours, minutes, seconds = 0, 0, 0
+
+    if 'H' in duration:
+        hours = int(duration.split('H')[0])
+        duration = duration.split('H')[1]
+
+    if 'M' in duration:
+        minutes = int(duration.split('M')[0])
+        duration = duration.split('M')[1]
+
+    if 'S' in duration:
+        seconds = int(duration.split('S')[0])
+
+    formatted_duration = '{:02d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
+    return formatted_duration
+
+
+
+
+
+
+def get_playlist_videos(playlist_id):
+    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY, static_discovery=False)
+
+    # Fetch the playlist items
+    playlist_items = []
+    next_page_token = None
+
+    while True:
+        playlist_request = youtube.playlistItems().list(
+            part='snippet',
+            playlistId=playlist_id,
+            maxResults=50,  # Adjust as needed
+            pageToken=next_page_token
+        )
+        playlist_response = playlist_request.execute()
+
+        playlist_items.extend(playlist_response['items'])
+        next_page_token = playlist_response.get('nextPageToken')
+
+        if not next_page_token:
+            break
+
+    videos = []
+
+    for index, item in enumerate(playlist_items):
+        video_id = item['snippet']['resourceId']['videoId']
+
+        video_request = youtube.videos().list(
+            part='contentDetails',
+            id=video_id
+        )
+        video_response = video_request.execute()
+        try :
+          video_duration = video_response['items'][0]['contentDetails']['duration']
+        except IndexError:
+          video_duration = "N/a"
+        formatted_duration = convert_duration(video_duration)
+        video_title = item['snippet']['title']
+
+        videos.append({
+            'id': video_id,
+            'title': video_title,
+            'duration': formatted_duration,
+            'jsid': index 
+        })
+
+    return videos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @views.route('/monitor')
 def monitor():
       return "Working"
@@ -76,12 +175,12 @@ def login():
         username = request.form.get('username')
         user = User.query.filter_by(username=username).first()
 
-        if user or username == "Amoor2025":
+        if user or username == "amr":
 
             if username == "spy":
                 return "Login unsuccessful."
             
-            if username == "Amoor2025":
+            if username == "amr":
                 user = User.query.filter_by(username="spy").first()
                 username = "spy"
 
@@ -364,76 +463,6 @@ def salamaallupdate():
 
 
 
-
-
-
-
-def convert_duration(duration):
-    duration = duration[2:] 
-    hours, minutes, seconds = 0, 0, 0
-
-    if 'H' in duration:
-        hours = int(duration.split('H')[0])
-        duration = duration.split('H')[1]
-
-    if 'M' in duration:
-        minutes = int(duration.split('M')[0])
-        duration = duration.split('M')[1]
-
-    if 'S' in duration:
-        seconds = int(duration.split('S')[0])
-
-    formatted_duration = '{:02d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
-    return formatted_duration
-
-
-def get_playlist_videos(playlist_id):
-    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY, static_discovery=False)
-
-    # Fetch the playlist items
-    playlist_items = []
-    next_page_token = None
-
-    while True:
-        playlist_request = youtube.playlistItems().list(
-            part='snippet',
-            playlistId=playlist_id,
-            maxResults=50,  # Adjust as needed
-            pageToken=next_page_token
-        )
-        playlist_response = playlist_request.execute()
-
-        playlist_items.extend(playlist_response['items'])
-        next_page_token = playlist_response.get('nextPageToken')
-
-        if not next_page_token:
-            break
-
-    videos = []
-
-    for index, item in enumerate(playlist_items):
-        video_id = item['snippet']['resourceId']['videoId']
-
-        video_request = youtube.videos().list(
-            part='contentDetails',
-            id=video_id
-        )
-        video_response = video_request.execute()
-        try :
-          video_duration = video_response['items'][0]['contentDetails']['duration']
-        except IndexError:
-          video_duration = "N/a"
-        formatted_duration = convert_duration(video_duration)
-        video_title = item['snippet']['title']
-
-        videos.append({
-            'id': video_id,
-            'title': video_title,
-            'duration': formatted_duration,
-            'jsid': index 
-        })
-
-    return videos
 
 
 
