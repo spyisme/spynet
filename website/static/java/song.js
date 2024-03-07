4b2aa9f269a55294163cbda61768b1ad0885f8fe
Spyisme <amooraymanh73007@gmail.com>
2024-03-07 13:43:00 +0200

    ff

--- website/static/java/song.js
+++ website/static/java/song.js
@@ -1,26 +1,21 @@
-// static/script.js
-document.addEventListener("DOMContentLoaded", function () {
-    var audio = document.getElementById("backgroundAudio");
+var audio = document.getElementById("backgroundAudio");
 
-    // Check if the audio element exists on the page
-    if (audio) {
-        // Play the audio as soon as the page loads
-        audio.volume = 0.5;
+// Check if the audio element exists on the page
+if (audio) {
+    // Play the audio as soon as the script loads
+    audio.volume = 0.5;
+    audio.play();
 
-        audio.play();
+    // Save the current time when leaving the page
+    window.addEventListener("beforeunload", function () {
+        localStorage.setItem("audioTime", audio.currentTime);
+    });
 
-        // Save the current time when leaving the page
-        window.addEventListener("beforeunload", function () {
-            localStorage.setItem("audioTime", audio.currentTime);
-        });
-
-        // Load the saved time when entering the page
-        window.addEventListener("load", function () {
-            var savedTime = localStorage.getItem("audioTime");
-            if (savedTime) {
-
-                audio.currentTime = parseFloat(savedTime);
-            }
-        });
-    }
-});
+    // Load the saved time when entering the page
+    window.addEventListener("load", function () {
+        var savedTime = localStorage.getItem("audioTime");
+        if (savedTime) {
+            audio.currentTime = parseFloat(savedTime);
+        }
+    });
+}
