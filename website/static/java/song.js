734bd8b2110af23493231e0a092fc12bd97efd34
Spyisme <amooraymanh73007@gmail.com>
2024-03-07 13:25:29 +0200

    ff

---
+++ website/static/java/song.js
@@ -0,0 +1,23 @@
+// static/script.js
+document.addEventListener("DOMContentLoaded", function () {
+    var audio = document.getElementById("backgroundAudio");
+
+    // Check if the audio element exists on the page
+    if (audio) {
+        // Play the audio as soon as the page loads
+        audio.play();
+
+        // Save the current time when leaving the page
+        window.addEventListener("beforeunload", function () {
+            localStorage.setItem("audioTime", audio.currentTime);
+        });
+
+        // Load the saved time when entering the page
+        window.addEventListener("load", function () {
+            var savedTime = localStorage.getItem("audioTime");
+            if (savedTime) {
+                audio.currentTime = parseFloat(savedTime);
+            }
+        });
+    }
+});
