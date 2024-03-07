1e72740d5951e617d61989d132c0f9ac829eead6
Spyisme <amooraymanh73007@gmail.com>
2024-03-07 16:42:43 +0200

    ff

--- website/static/java/song.js
+++ website/static/java/song.js
@@ -1,10 +1,16 @@
+// Song.js
 var audio = document.getElementById("backgroundAudio");
 
 // Check if the audio element exists on the page
 if (audio) {
-    // Play the audio as soon as the script loads
-    audio.volume = 0.5;
-    audio.play();
+    // Load the music state from local storage
+    var musicOn = localStorage.getItem("musicOn");
+
+    if (musicOn === "true") {
+        // Play the audio if music is on
+        audio.volume = 0.5;
+        audio.play();
+    }
 
     // Save the current time when leaving the page
     window.addEventListener("beforeunload", function () {
@@ -28,10 +34,20 @@ if (audio) {
     });
 }
 
-function playAudio() {
+function toggleMusic() {
     if (audio) {
-        audio.volume = 0.5;
-        audio.play();
+        var musicOn = localStorage.getItem("musicOn");
+
+        if (musicOn === "true") {
+            // Turn off the music
+            audio.pause();
+            localStorage.setItem("musicOn", "false");
+        } else {
+            // Turn on the music
+            audio.volume = 0.5;
+            audio.play();
+            localStorage.setItem("musicOn", "true");
+        }
 
         // Save the current time when leaving the page
         window.addEventListener("beforeunload", function () {
@@ -40,8 +56,9 @@ function playAudio() {
     }
 }
 
+// Add an event listener for the "click" event to toggle music
 document.addEventListener("click", function () {
-    playAudio();
-    // Remove the click event listener after playing the audio
-    document.removeEventListener("click", playAudio);
+    toggleMusic();
+    // Remove the click event listener after toggling music
+    document.removeEventListener("click", toggleMusic);
 });
