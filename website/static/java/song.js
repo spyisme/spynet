8857d4abc8849e6b1505451cb87947860d0f1b2b
Spyisme <amooraymanh73007@gmail.com>
2024-03-07 16:46:43 +0200

    ff

--- website/static/java/song.js
+++ website/static/java/song.js
@@ -58,46 +58,16 @@ function toggleMusic() {
 
 // Add an event listener for the "click" event to toggle music
 document.addEventListener("click", function () {
-    playAudio();
-    
-    // Remove the click event listener after toggling music
-    document.removeEventListener("click", playAudio);
-});
-
-
-function playAudio() {
-
-    // Check if the audio element exists on the page
-if (audio) {
-    // Load the music state from local storage
     var musicOn = localStorage.getItem("musicOn");
 
+    // Check if music is set to true and play it
     if (musicOn === "true") {
-        // Play the audio if music is on
         audio.volume = 0.5;
         audio.play();
+
+        // Save the current time when leaving the page
+        window.addEventListener("beforeunload", function () {
+            localStorage.setItem("audioTime", audio.currentTime);
+        });
     }
-
-    // Save the current time when leaving the page
-    window.addEventListener("beforeunload", function () {
-        localStorage.setItem("audioTime", audio.currentTime);
-    });
-
-    // Load the saved time when entering the page
-    window.addEventListener("load", function () {
-        var savedTime = localStorage.getItem("audioTime");
-        if (savedTime) {
-            audio.currentTime = parseFloat(savedTime);
-        }
-    });
-
-    // Add an event listener for the "ended" event
-    audio.addEventListener("ended", function () {
-        // Refresh the src to play a new song or restart the same one
-        audio.src = "/random_song";
-        audio.load();
-        audio.play();
-    });
-}
-
-}
+});
