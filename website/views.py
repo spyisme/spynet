bd77c5996baa6ca5b57d844c137954d6c2e8f65e
Spyisme <amooraymanh73007@gmail.com>
2024-03-11 23:12:44 +0200

    ff

--- website/views.py
+++ website/views.py
@@ -1194,21 +1194,32 @@ def salama():
 def salamach(custom_url):
     extra = None
     salama_info = load_salama_info()
+    
+    # Find the course based on the provided custom URL
     course_info = next((info for info in salama_info.values() if info['url'] == f"/salama/{custom_url}"), None)
-    course_name = next((name for name, info in salama_info.items() if info['url'] == f"/salama/{custom_url}"), None)
-    teachername = course_name
-    playlist_id = course_info["id"]
-    with open(f"website/playlists/salama{course_name}.txt", 'r', encoding='utf-8') as file:
+
+    if course_info:
+        # Retrieve other information as needed
+        course_name = next((name for name, info in salama_info.items() if info['url'] == f"/salama/{custom_url}"), None)
+        teachername = course_name
+        playlist_id = course_info.get("id", "")  # Use .get() to avoid NoneType error
+
+        with open(f"website/playlists/salama{course_name}.txt", 'r', encoding='utf-8') as file:
             content = file.read()
             videos = ast.literal_eval(content)
-    if course_name  == "Course 6" :
-       extra={"Pdf 1" :"https://drive.google.com/file/d/18mnyKrmeiNNZMBdaJ0sD8VAkAzLbM15r/view?usp=drive_link" , "Pdf 2" : "https://drive.google.com/file/d/1JLwNyWB8lOVSdVi8IvA6zb1D1iVQ8H3l/view?usp=drive_link"}   
-    elif course_name  == "Course 17" :
-        extra = {"Pdf 1" : "https://drive.google.com/file/d/1Ng8UkfF48_Cj1ZjiMn8NPfkWEONh3vJD/view?usp=drive_link"}
-    elif course_name  == "Course 19":
-        extra={"Pdf 1" :"https://drive.google.com/file/d/1a-56mRMP3nYSts90itOfINMtmrb8z6rr/view?usp=drive_link" , "Pdf 2" : "https://drive.google.com/file/d/1O21TqOmEJv2R9zUJmMw0BFnHsjCtqEVF/view?usp=drive_link"}   
-    return render_template('used_pages/videopage.html', videos=videos, playlist_id=playlist_id, teachername=teachername ,extra = extra)
 
+        # Handle any additional data based on course
+        extra = None
+        if course_name == "Course 6":
+            extra = {"Pdf 1": "https://drive.google.com/file/d/18mnyKrmeiNNZMBdaJ0sD8VAkAzLbM15r/view?usp=drive_link",
+                     "Pdf 2": "https://drive.google.com/file/d/1JLwNyWB8lOVSdVi8IvA6zb1D1iVQ8H3l/view?usp=drive_link"}
+        elif course_name == "Course 17":
+            extra = {"Pdf 1": "https://drive.google.com/file/d/1Ng8UkfF48_Cj1ZjiMn8NPfkWEONh3vJD/view?usp=drive_link"}
+        elif course_name == "Course 19":
+            extra = {"Pdf 1": "https://drive.google.com/file/d/1a-56mRMP3nYSts90itOfINMtmrb8z6rr/view?usp=drive_link",
+                     "Pdf 2": "https://drive.google.com/file/d/1O21TqOmEJv2R9zUJmMw0BFnHsjCtqEVF/view?usp=drive_link"}
+
+        return render_template('used_pages/videopage.html', videos=videos, playlist_id=playlist_id, teachername=teachername, extra=extra)
 
 
 #Arabic --------------------------------------------------------------------------------------------------------------------------
