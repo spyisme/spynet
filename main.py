d6437c0903e7f82c07ba02dc0d7a1ff9f12b3cd9
Tylerisme <156155248+Tylerisme@users.noreply.github.com>
2024-03-30 06:11:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-from flask import Flask, redirect  # Import Flask and redirect from Flask module
 from website import create_app
 
-# Create Flask app instance
-app, socketio = create_app()
+from flask import redirect
+import logging
 
-# Define error handler for 404 Not Found error
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+app , socketio = create_app()
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect('/')
 
-if __name__ == "__main__":
-    # Use Gunicorn to serve the Flask app
-    import os
-    host = os.environ.get('HOST', '0.0.0.0')
-    port = int(os.environ.get('PORT', 80))
-    workers = int(os.environ.get('WORKERS', 4))
 
-    socketio.run(app, host=host, port=port, debug=True)
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=80, allow_unsafe_werkzeug=True , debug=True)
+
+
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+
+
+#     print(request.headers)
+
+#     print(request.url)
+#     return f"404 Error - User tried to access: {request.url}"
