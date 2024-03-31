957ef9553737ab58e63551f43095800245c4fe7e
Spyisme <amooraymanh73007@gmail.com>
2024-03-31 06:11:58 +0200

    ss

--- website/__init__.py
+++ website/__init__.py
@@ -25,7 +25,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'amooraymanh730072@gmail.com'
     app.config['MAIL_PASSWORD'] = 'ocpb mxsf ncwu pebf'
-    app.config['MAIL_DEFAULT_SENDER'] = 'no-reply@spysnet.com'
+    app.config['MAIL_DEFAULT_SENDER'] = 'sanawyasessions@spysnet.com'
 
     mail.init_app(app)
 
