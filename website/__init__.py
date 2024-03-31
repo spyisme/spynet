4998f31df29f42dcf17721390f4fd42aae24c405
Spyisme <amooraymanh73007@gmail.com>
2024-03-31 08:56:19 +0200

    ss

--- website/__init__.py
+++ website/__init__.py
@@ -137,19 +137,6 @@ def create_app():
                             
                         discord_log(f"{client_ip} Viewed <{request.path}>  {current_user.username} Device ```{user_agent}```")
 
-        if request.path.startswith('/redirect/'):
-            request.path = request.path.split('/')
-            request.path = '/'.join( request.path[2:])
-            request.path =  request.path.replace('questionmark', '?')
-            request.path =  request.path.replace('andsympol', '&')
-        else :
-            request.path = f"https://spysnet.com{request.path}"
-        client_ip = request.headers.get('CF-Connecting-IP', request.remote_addr)
-        user_agent = request.headers.get('User-Agent')
-        if not current_user.is_authenticated :
-            current_user.username =  'Not logged'
-        discord_log_unfiltered(f"{client_ip} Viewed <{request.path}> { current_user.username} Device ```{user_agent}```")
-
 
             
     app.before_request(before_request)
