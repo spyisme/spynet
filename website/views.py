7b9c1d66b564f8d6c34c4d9da85917884425039c
Spyisme <amooraymanh73007@gmail.com>
2024-03-31 23:40:13 +0200

    ss

--- website/views.py
+++ website/views.py
@@ -290,7 +290,7 @@ def login():
 
         if user :
 
-            if (user.username != "spy" and user.username != "biba") and user.active_sessions >= 2 :
+            if (username != "spy" and user.username != "biba") and user.active_sessions >= 2 :
 
                 discord_log_login(f"{username} tried to login from more than 2 devices <@709799648143081483>")
                 return redirect("/login?maxdevices=true")
