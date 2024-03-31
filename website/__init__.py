046665091466bf712dfb4aafc7630213422e5999
Spyisme <amooraymanh73007@gmail.com>
2024-03-31 06:03:26 +0200

    ss

--- website/__init__.py
+++ website/__init__.py
@@ -26,7 +26,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'amooraymanh730072'
     app.config['MAIL_PASSWORD'] = 'ocpb mxsf ncwu pebf'
-
+    app.config['MAIL_DEFAULT_SENDER'] = 'no-reply@spysnet.com'
     
     @app.route('/refreshpage')
     def refresh_page():
