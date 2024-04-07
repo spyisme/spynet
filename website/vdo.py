946b5cdd98e1e6c966561ab5a0ae11d5587dd146
Spyisme <amooraymanh73007@gmail.com>
2024-04-07 15:46:40 +0200

    ss

--- website/vdo.py
+++ website/vdo.py
@@ -188,7 +188,7 @@ def index():
             }
             r = requests.post(link, json=payload_new, headers=headers())
             print(r)
-            discord_log(r)
+            discord_log(r.json())
             return r.json()['license']
     
         def start(self):
