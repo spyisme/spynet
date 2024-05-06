007df092271e1837a54747bdd86fe735f4142ca4
Spyisme <amooraymanh73007@gmail.com>
2024-05-07 01:51:06 +0300

    ss

--- website/static/java/countdown.js
+++ website/static/java/countdown.js
@@ -1,30 +1,36 @@
+function startCountdown() {
+    // Initial call to update countdown
+    updateCountdown();
 
-var countDownDate = new Date("June 10, 2024 08:00:00 GMT+0200").getTime();
+    // Set interval to update countdown every second
+    var x = setInterval(updateCountdown, 1000);
 
-// Update the countdown every 1 second
-var x = setInterval(function() {
+    function updateCountdown() {
+        var countDownDate = new Date("June 10, 2024 08:00:00 GMT+0200").getTime();
 
-// Get the current date and time
-var now = new Date().getTime();
+        // Get the current date and time
+        var now = new Date().getTime();
 
-// Calculate the distance between now and the countdown date
-var distance = countDownDate - now;
+        // Calculate the distance between now and the countdown date
+        var distance = countDownDate - now;
 
-// Calculate days, hours, minutes and seconds
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Calculate days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-// Output the result in an element with id="countdown"
-document.getElementById("countdown").innerHTML = "Days till exams: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        // Output the result in an element with id="countdown"
+        document.getElementById("countdown").innerHTML = "Days till exams: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-// If the countdown is over, display a message
-if (distance < 0) {
-clearInterval(x);
-document.getElementById("countdown").innerHTML = "EXPIRED";
+        // If the countdown is over, display a message
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }
 }
-}, 1000);
+
 // JavaScript code to adjust content padding to prevent it from overlapping with the fixed footer
 function adjustContentPadding() {
     var content = document.querySelector('.content');
