9ccdcd3a4d7da03c9b7a0a10b79ef831918072e8
Spyisme <amooraymanh73007@gmail.com>
2024-06-02 22:56:47 +0300

    ss

--- website/views.py
+++ website/views.py
@@ -937,37 +937,37 @@ def samehtest():
 
 @views.route('/ashrafsessions')
 def ashrafsessions():
-    # headers = {
-    #     'authority': 'api.csacademyzone.com',
-    #     'accept': 'application/json, text/plain, */*',
-    # }
-    # json_data = {
-    #     'active': 1,
-    # }
+    headers = {
+        'authority': 'api.csacademyzone.com',
+        'accept': 'application/json, text/plain, */*',
+    }
+    json_data = {
+        'active': 1,
+    }
 
-    # response = requests.post('https://api.csacademyzone.com/lectures', headers=headers, json=json_data)
+    response = requests.post('https://api.csacademyzone.com/lectures', headers=headers, json=json_data)
+    return response
 
-
-    # data = response.json()
-    # filtered_lectures = []
-    # last_id = None
+    data = response.json()
+    filtered_lectures = []
+    last_id = None
     
-    # for lecture in data['lectures']:
-    #     filtered_lecture = {
-    #         "id": lecture["id"],
-    #         "title": lecture["title"]
-    #     }
-    #     for part in ascii_lowercase:
-    #         part_key = f"part_{part}_video"
-    #         if part_key in lecture and lecture[part_key]:
-    #             filtered_lecture[part_key] = lecture[part_key]
-    #     filtered_lectures.append(filtered_lecture)
-    #     last_id = lecture["id"] 
+    for lecture in data['lectures']:
+        filtered_lecture = {
+            "id": lecture["id"],
+            "title": lecture["title"]
+        }
+        for part in ascii_lowercase:
+            part_key = f"part_{part}_video"
+            if part_key in lecture and lecture[part_key]:
+                filtered_lecture[part_key] = lecture[part_key]
+        filtered_lectures.append(filtered_lecture)
+        last_id = lecture["id"] 
 
-    # result = {"filtered_lectures": filtered_lectures}
+    result = {"filtered_lectures": filtered_lectures}
 
-    # with open("website/Backend/ashraf.json", 'w') as output_file:
-    #     json.dump(result, output_file, indent=2)
+    with open("website/Backend/ashraf.json", 'w') as output_file:
+        json.dump(result, output_file, indent=2)
 
     try:
         with open('website/Backend/ashraf.json', encoding='utf-8') as file:
