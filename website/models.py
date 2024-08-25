a0647d409592ba806df63b5ace2c635b8e88d196
Spyisme <amooraymanh73007@gmail.com>
2024-08-25 16:46:17 +0300

    ss

--- website/models.py
+++ website/models.py
@@ -10,7 +10,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=True)
     otp = db.Column(db.Integer)
     stage = db.Column(db.String(50), nullable=True, default=3)
-    type = db.Column(db.String(50), default="student_admin")
+    type = db.Column(db.String(50), default="student")
+    created_by = db.Column(db.String(50), nullable=True)
     logs = db.Column(db.Text, nullable=True, default='[]')  # Store logs as JSON string
     subscription_date = db.Column(db.Date, nullable=True)
     subscription_method = db.Column(db.String(50), default="None")
