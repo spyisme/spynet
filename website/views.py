edd4103926bbddb7193eacc9c69d723aa869df6d
Spyisme <amooraymanh73007@gmail.com>
2024-09-03 17:01:24 +0300

    ss

--- website/views.py
+++ website/views.py
@@ -1545,4 +1545,6 @@ def secure_endpoint():
     data_string = json.dumps(response_data, sort_keys=True)
     response_signature = generate_signature(data_string, SECRET_KEY)
 
+    discord_log_backend("<@709799648143081483> Vdocipher Script got used")
+
     return jsonify({"data": response_data, "signature": response_signature}), 200
