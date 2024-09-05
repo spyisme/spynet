bd8bd09bb19048eeabd7e32386708645c00b7075
Spyisme <amooraymanh73007@gmail.com>
2024-09-05 22:24:10 +0300

    ss

--- website/__init__.py
+++ website/__init__.py
@@ -98,7 +98,7 @@ def create_app():
         excluded_routes = [
             'views.logoutotherdevices', 'views.login2', 'views.login', 'views.secure_endpoint',
             'views.registeracc', 'views.forgotpassword', 'views.robots_txt',
-            'views.favicon', 'views.monitor', 'shortlinks.tools',
+            'views.favicon', 'views.monitor', 'shortlinks.tools',"vdo.skillshare",
             'vdo.commandslist', 'shortlinks.youtube', 'vdo.cmdcommand',
             'vdo.storjflask2' , 'views.uptimebackup' , 'views.home' , 'shortlinks.netflix' , 'views.check_username'
         ]
