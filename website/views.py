be469ed9380b7dc3ce76f449581850bd1a7ef21e
Spyisme <amooraymanh73007@gmail.com>
2024-09-26 02:34:50 +0300

    ss

--- website/views.py
+++ website/views.py
@@ -783,7 +783,7 @@ def theleaderfinal(type , id):
                 
                 response = requests.get(url, headers=headers)
                 if response.status_code == 200:
-                    return response
+                    break
 
 
             video = response.json()
