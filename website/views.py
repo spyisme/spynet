4fe7dfd8a7f3d1d294b43dd6b221ac4002122413
spyisme <amooraymanh730072@gmail.com>
2024-10-30 12:46:27 +0300

    ss

--- website/views.py
+++ website/views.py
@@ -1764,12 +1764,11 @@ def english_assignment():
             if image.mode in ("RGBA", "P"):
                 image = image.convert("RGB")
             pdf_path = f"website/static/english/img{file_name}.pdf"
-            
+
             image.save(pdf_path, "PDF", resolution=100.0)
 
-            send_file(f"static/english/img{file_name}.pdf")
+            return send_file(f"static/english/img{file_name}.pdf")
 
-            return redirect(f"/static/english/img{file_name}.png")
         else :
             return render_template("used_pages/english_assignment_error.html")
 
