13ffd3e12f744177fb84bcda7ec9ff546fdfe673
spyisme <amooraymanh730072@gmail.com>
2024-12-02 23:54:53 +0200

    ss

--- website/views.py
+++ website/views.py
@@ -2691,4 +2691,35 @@ def computerpdfs():
 
 
 
+shortlinks_file = 'website/Backend/shortlinks.json'
 
+def load_shortlinks():
+    with open(shortlinks_file, 'r') as f:
+        return json.load(f)
+    return {}
+
+
+def save_shortlinks(shortlinks):
+    with open(shortlinks_file, 'w') as f:
+        json.dump(shortlinks, f, indent=4)
+
+@views.route("/short/manage", methods=["GET", "POST"])
+def manage_urls():
+    if request.method == "POST":
+
+        action = request.form.get("action")
+        key = request.form.get("key")
+        url = request.form.get("url")
+        
+        shortlinks = load_shortlinks()
+
+        if action == "add" and key and url:
+            shortlinks[key] = url
+            save_shortlinks(shortlinks)
+        elif action == "delete" and key:
+            if key in shortlinks:
+                del shortlinks[key]
+                save_shortlinks(shortlinks)
+
+    shortlinks = load_shortlinks()
+    return render_template('admin/shortlinks.html', shortlinks=shortlinks)
