3e10f14c82c7ee25b65ee52cec5586a295f7b8c2
spyisme <amooraymanh730072@gmail.com>
2024-12-12 21:44:42 +0200

    ss

--- website/ecu.py
+++ website/ecu.py
@@ -57,7 +57,8 @@ chatgptnonce = {"nonce": "Not set yet!"}
 def english_assignment():
 
     if not current_user.is_authenticated :
-            if current_user.username != "spy" :
+            return render_template("ecu/english_assignment_off.html")
+    elif current_user.username != "spy" :
                 return render_template("ecu/english_assignment_off.html")
 
 
