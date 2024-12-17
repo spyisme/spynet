bc5f0b49a31b9884021d97572a57e4f2c16fef40
spyisme <amooraymanh730072@gmail.com>
2024-12-17 21:35:20 +0200

    ss

--- website/ecu.py
+++ website/ecu.py
@@ -325,7 +325,7 @@ def english_assignment():
 
                 json_data = response.json()
 
-
+            discord_log_english(f"{json_data["reply"]}")
             data = json.loads(json_data["reply"])
             key = next((k for k in data.keys() if k.lower() in ['words', 'word']), None)
 
@@ -567,6 +567,7 @@ def english_assignment():
     #-----------------------------------------------------------------------------------------------------------------
     
             elif assignment == '5to7' or assignment == '8to10' :
+                discord_log_english("Assignment 5ot7 or 8to10")
                 fields_data_5to7 = {
                     "Today's POWER WORD 1": {"text": "", "coords": (955,973), "max_x": 1618, "max_y": 1243},  
                     "Definition 1": {"text": "", "coords": (99,845), "max_x": 805, "max_y": 1291},
@@ -641,8 +642,12 @@ def english_assignment():
                         api = "2"
                         get_image(search_term, filename , api)
 
+                discord_log_english("Everything ready")
+
 
                 def make_image_final(name , max_font_size ,min_font_size , img_1_cords):
+                    discord_log_english("Started")
+
                     start_time = time.time()
 
                     # discord_log_english("Making the final image...")
