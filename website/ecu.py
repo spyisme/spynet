3d8d3e474b0976d4c3158db0ccac9903916cb935
spyisme <amooraymanh730072@gmail.com>
2024-12-23 22:03:54 +0200

    ss

--- website/ecu.py
+++ website/ecu.py
@@ -1157,10 +1157,14 @@ def ecu_search():
     query = request.form.get('query', '').strip().lower()
     results = []
     search = 0
+
+    max_search = 15
+
+    #Extra for me
     if current_user.is_authenticated:
         if current_user.username == 'spy' :
             max_search = 999999
-    # Load JSON data
+
     with open('website/Backend/ECU/ECU24~23.json', 'r') as f:
         data2 = json.load(f)
 
